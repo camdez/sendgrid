669cd54057362c6b3f3e913a52192c6a81c89603
Cameron Desautels <camdez@gmail.com>
2021-03-24 14:18:24 -0500

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/camdez/sendgrid"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dependencies [[cheshire "5.5.0"]
-                 [clj-http "2.0.1"]
-                 [environ "1.0.2"]
-                 [org.clojure/clojure "1.7.0"]]
+  :dependencies [[cheshire "5.10.0"]
+                 [clj-http "3.12.1"]
+                 [environ "1.2.0"]
+                 [org.clojure/clojure "1.10.3"]]
   :plugins [[lein-environ "1.0.2"]])
