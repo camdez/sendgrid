6abfbac970830ea96427823536e0a1288eab205e
Cameron Desautels <camdez@gmail.com>
2022-12-19 15:23:52 -0600

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject camdez/sendgrid "0.2.1-SNAPSHOT"
+(defproject camdez/sendgrid "0.2.1"
   :description "Clojure library for sending emails with SendGrid"
   :url "https://github.com/camdez/sendgrid"
   :license {:name "MIT License"
